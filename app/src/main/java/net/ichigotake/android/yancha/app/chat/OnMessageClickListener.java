a9c9122c8153dbe5a7167acded06b189094f57a3
ichigotake <ichigotake.san@gmail.com>
2014-09-17 00:20:18 +0900

    全て無に帰そう

--- app/src/main/java/net/ichigotake/android/yancha/app/chat/OnMessageClickListener.java
+++
@@ -1,8 +0,0 @@
-package net.ichigotake.android.yancha.app.chat;
-
-import net.ichigotake.yancha.sdk.chat.ChatMessage;
-
-public interface OnMessageClickListener {
-
-    void onClick(int position, ChatMessage item);
-}
